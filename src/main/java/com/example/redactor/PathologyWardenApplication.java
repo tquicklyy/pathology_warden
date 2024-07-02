107c44502d867fe602e1b16d02d89fbee5ef8048
Gleb <tquicklyy@yandex.ru>
2024-07-03 00:54:29 +0300

    Improved scaling. When switching between screens with different scales, the application window is displayed correctly

--- src/main/java/com/example/redactor/PathologyWardenApplication.java
+++ src/main/java/com/example/redactor/PathologyWardenApplication.java
@@ -1,31 +1,61 @@
 package com.example.redactor;
-
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 import java.io.IOException;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PathologyWardenApplication extends Application {
+    static double MIN_WIDTH_STAGE = 1214.4000244140625;
+    static double MIN_HEIGHT_STAGE = 737.5999755859375;
+    private Timer timer = new Timer();
+    private boolean isDragging = false;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PathologyWardenApplication.class.getResource("scene-main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.getIcons().add(new Image("file:./src/main/resources/images/logo-pathology-warden.png"));
         stage.setTitle("Pathology Warden");
         stage.setScene(scene);
         stage.show();
+        stage.setMinWidth(MIN_WIDTH_STAGE);
+        stage.setMinHeight(MIN_HEIGHT_STAGE);
 
+        ChangeListener<Number> positionChangeListener = new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (!isDragging) {
+                    isDragging = true;
+                    stage.setMinWidth(0);
+                    stage.setMinHeight(0);
+                }
+                resetDragEndTimer(stage);
+            }
+        };
+        stage.xProperty().addListener(positionChangeListener);
+        stage.yProperty().addListener(positionChangeListener);
+    }
 
-        // Нужно добавить событие на переход между мониторами с разным масштабом экрана(на мень)
-        stage.setMinWidth(1214.4000244140625);
-        stage.setMinHeight(737.5999755859375);
+    private void resetDragEndTimer(Stage stage) {
+        timer.cancel();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    isDragging = false;
+                    stage.setMinWidth(MIN_WIDTH_STAGE);
+                    stage.setMinHeight(MIN_HEIGHT_STAGE);
+                });
+            }
+        }, 100);
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.redactor {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.redactor to javafx.fxml;
