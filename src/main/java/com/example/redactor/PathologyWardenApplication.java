f5c6eaed21e4da9732f9ae3ee9ffb52b8d40082a
Gleb <tquicklyy@yandex.ru>
2024-07-03 15:24:48 +0300

    Improved readability renamed some variables, methods, timer and added lambda expressions

--- src/main/java/com/example/redactor/PathologyWardenApplication.java
+++ src/main/java/com/example/redactor/PathologyWardenApplication.java
@@ -2,7 +2,6 @@ package com.example.redactor;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -12,45 +11,42 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class PathologyWardenApplication extends Application {
-    static double MIN_WIDTH_STAGE = 1214.4000244140625;
-    static double MIN_HEIGHT_STAGE = 737.5999755859375;
-    private Timer timer = new Timer();
-    private boolean isDragging = false;
+    final double MIN_WIDTH_STAGE = 1214.4000244140625;
+    final double MIN_HEIGHT_STAGE = 737.5999755859375;
+    private Timer timerForMainMenuStageDragging = new Timer();
+    private boolean isMainMenuStageDragging = false;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage mainMenuStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PathologyWardenApplication.class.getResource("scene-main-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
-        stage.getIcons().add(new Image("file:./src/main/resources/images/logo-pathology-warden.png"));
-        stage.setTitle("Pathology Warden");
-        stage.setScene(scene);
-        stage.show();
-        stage.setMinWidth(MIN_WIDTH_STAGE);
-        stage.setMinHeight(MIN_HEIGHT_STAGE);
+        mainMenuStage.getIcons().add(new Image("file:./src/main/resources/images/logo-pathology-warden.png"));
+        mainMenuStage.setTitle("Pathology Warden");
+        mainMenuStage.setScene(scene);
+        mainMenuStage.show();
+        mainMenuStage.setMinWidth(MIN_WIDTH_STAGE);
+        mainMenuStage.setMinHeight(MIN_HEIGHT_STAGE);
 
-        ChangeListener<Number> positionChangeListener = new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (!isDragging) {
-                    isDragging = true;
-                    stage.setMinWidth(0);
-                    stage.setMinHeight(0);
-                }
-                resetDragEndTimer(stage);
+        ChangeListener<Number> mainMenuStagePositionChangeListener = (observable, oldValue, newValue) -> {
+            if (!isMainMenuStageDragging) {
+                isMainMenuStageDragging = true;
+                mainMenuStage.setMinWidth(0);
+                mainMenuStage.setMinHeight(0);
             }
+            resetTimerForMainMenuStageDragging(mainMenuStage);
         };
-        stage.xProperty().addListener(positionChangeListener);
-        stage.yProperty().addListener(positionChangeListener);
+        mainMenuStage.xProperty().addListener(mainMenuStagePositionChangeListener);
+        mainMenuStage.yProperty().addListener(mainMenuStagePositionChangeListener);
     }
 
-    private void resetDragEndTimer(Stage stage) {
-        timer.cancel();
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+    private void resetTimerForMainMenuStageDragging(Stage stage) {
+        timerForMainMenuStageDragging.cancel();
+        timerForMainMenuStageDragging = new Timer();
+        timerForMainMenuStageDragging.schedule(new TimerTask() {
             @Override
             public void run() {
                 Platform.runLater(() -> {
-                    isDragging = false;
+                    isMainMenuStageDragging = false;
                     stage.setMinWidth(MIN_WIDTH_STAGE);
                     stage.setMinHeight(MIN_HEIGHT_STAGE);
                 });
